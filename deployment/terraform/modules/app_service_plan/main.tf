03c0e1aa715228a4fafc0a6e1e2320f42d593404
AshishArora24 <ashisharora24@outlook.com>
2026-05-19 11:18:46 +0000

    feat: add app_service_plan terraform module

---
+++ deployment/terraform/modules/app_service_plan/main.tf
@@ -0,0 +1,13 @@
+resource "azurerm_service_plan" "this" {
+  name                = "asp-${var.project}-${var.deployment_type}-${var.environment}"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  os_type             = "Linux"
+  sku_name            = var.sku_name
+
+  tags = {
+    project         = "infrapilot"
+    deployment_type = var.deployment_type
+    environment     = var.environment
+  }
+}
